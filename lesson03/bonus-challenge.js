ac963137a6e720fca967ac4c0c0ad89fc2b8299b
nbolinaga <nbolinaga17@gmail.com>
2019-02-07 18:21:08 -0800

    added array length

--- lesson03/bonus-challenge.js
+++ lesson03/bonus-challenge.js
@@ -1,11 +1,11 @@
 // CARD DECK Lesson 03 Assignment 5 - Bonus
 
 // ARRAY
-let cards = [['of Hearts', 'of Diamonds', 'of Spades', 'of Clubs'],['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King']]
+let cards = [['of Hearts', 'of Diamonds', 'of Spades', 'of Clubs'] , ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King']]
 
 // LOOP
-for (let i = 0; i < 4; i++){
-  for (let j = 0; j < 13; j++){
+for (let i = 0; i < cards[0].length ; i++){
+  for (let j = 0; j < cards[1].length ; j++){
   console.log(`${cards[1][j]} ${cards[0][i]}`);
   }
 }
