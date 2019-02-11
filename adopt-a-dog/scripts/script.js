0dfbab01f7a7a114a5c8176e459b4a7d8f9a89e1
nbolinaga <nbolinaga17@gmail.com>
2019-02-10 23:57:45 -0800

    made it possible to unchoose dog and now alert messages appear separate

--- adopt-a-dog/scripts/script.js
+++ adopt-a-dog/scripts/script.js
@@ -11,17 +11,38 @@ function dogAdded(clicked_id) {
 // (decided to round to 2 decimals since it was resulting in $864.1500000000001 when ading the seventh dog. OCD i guess)
  alert(`You added ${clicked_id} to your adoption list. Your total is now: $${Number((checkoutTotal).toFixed(2))}`);
 // visually changes button once clicked
- document.getElementById(dogName).style.background = "tomato";
- // still dont know how to unpress button (unadopt)
- document.getElementById(dogName).innerHTML = "Unadopt";
- // button is no longer clickable
- document.getElementById(dogName).disabled = true;
+ document.getElementById(dogName).style.background = 'tomato';
+ document.getElementById(dogName).innerHTML = 'Unadopt';
+ // button will now execute unadopt function when pressed
+ document.getElementById(dogName).setAttribute('onclick', 'unadopt(this.id)');
+ // changes total indicator on the header
+ document.getElementById('cart').innerHTML = `<i class='fas fa-shopping-cart'></i>  $${Number((checkoutTotal).toFixed(2))}`;
 }
+function unadopt(clicked_id){
+// gets the dogs name by checking id
+ dogName = clicked_id;
+// substracts dog's price from your total
+ checkoutTotal = checkoutTotal - dogPrice;
+// alerts dog's name and new total
+// (decided to round to 2 decimals since it was resulting in $864.1500000000001 when ading the seventh dog. OCD i guess)
+ alert(`You removed ${clicked_id} from your adoption list. Your total is now: $${Number((checkoutTotal).toFixed(2))}`);
+// visually changes button once clicked
+ document.getElementById(dogName).style.background = '#81b741';
+ document.getElementById(dogName).innerHTML = 'Adopt';
+ // button will now execute dogAdded function when pressed
+ document.getElementById(dogName).setAttribute('onclick', 'dogAdded(this.id)');
+ // changes total indicator on the header
+ document.getElementById('cart').innerHTML = `<i class='fas fa-shopping-cart'></i>  $${Number((checkoutTotal).toFixed(2))}`;
+}
+
+
 
 // Discovered you can just alert an images alt text so why not use it as an alert
 function dogInfo(clicked_alt) {
 // gets dogs info by checking alt
  dogAlt = clicked_alt;
- // alerts dog's alt
- alert(dogAlt);
+ dogPoint = dogAlt.split('-');
+ dogPoint.forEach(function(item,index){
+  alert(`${item}`);
+  });
 }
